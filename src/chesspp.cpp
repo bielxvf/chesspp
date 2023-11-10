9152de4889e5987909de95f956b4159b93e99ed3
Biel Sala <biel.sala@autonoma.cat>
2023-11-10 14:53:51 +0100

    Abstract potential functions for printing the board

--- src/chesspp.cpp
+++ src/chesspp.cpp
@@ -13,9 +13,32 @@ const char initial_board[8][8] = { 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
 				 { 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
 				 { 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'};
 
+void print_board_white(char board[8][8]) {
+
+}
+
+void print_board_black(char board[8][8]) {
+
+}
+
+void print_board(char board[8][8], int color) {
+	switch (color) {
+		case WHITE:
+			print_board_white(board);
+			break;
+		case BLACK:
+			print_board_black(board);
+			break;
+		default:
+			break;
+	}
+}
+
 void new_game(void) {
-	std::cout << "Starting new game" << std::endl;
+	std::cout << "Choose your color: ";
+	std::cin >> color;
 	char board[8][8] = initial_board;
+	print_board(board, color);
 }
 
 int main(int argc, char** argv) {
